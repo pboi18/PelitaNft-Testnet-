45cb19483e1ff92144e4c50f613fe0d4d82e9f2b
Peter <peteresiet92@gmail.com>
2022-03-19 08:29:42 -0700

    update commit

--- contracts/PelitaNft.sol
+++ contracts/PelitaNft.sol
@@ -2,16 +2,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract PelitaNft is ERC721, Ownable {
+contract PelitaNft is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("PELITA", "PELNFT") {}
+    constructor()  ERC721("PELITA", "PELNFT") {}
 
     function mintNFT(address recipient, string memory tokenURI)
         public onlyOwner
